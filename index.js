53a130b3e52c9330f08cc948bb521e17676b24b9
Ellis <ellischung98@gmail.com>
2021-09-15 00:26:39 -0400

    craigslist scraping all working

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const CronJob = require('cron').CronJob;
 const nodemailer = require('nodemailer');
 
 async function getListings() {
+    // array to hold information for each of the listings
+    const listings = [];
+    
     const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
@@ -19,9 +22,6 @@ async function getListings() {
         return rows.map(row => {
             const properties = {};
             const titleElement = row.querySelector('.result-title');
-            //properties.title = titleElement.innerText;
-            //const priceElement = row.querySelector('.result-price');
-            //properties.price = priceElement ? priceElement.innerText : '';
             properties.url = titleElement.getAttribute('href');
             return properties;
         });
@@ -30,43 +30,58 @@ async function getListings() {
     // close first browser
     browser.close();
 
+    // get the information from the first 5 listings and add them to the array
     await Promise.all(results.slice(0,5).map(async result => {
-        getListingInfo(result.url);
+        const listing = await getListingInfo(result.url);
+        listings.push(listing);
     }));
 
-    //sendNotification(results.slice(0,5));
+    // send notification for new listings via email
+    sendNotification(listings);
 }
 
 async function getListingInfo(url) {
+    // holds all important information from the listing
+    const listing = {};
+
     const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
 
     await page.goto(url);
 
-    // holds all important information from the listing
-    const properties = {};
+    // title of listing
+    listing.title = await page.evaluate(() => {
+        const el = document.getElementById('titletextonly');
+        return el.textContent;
+    });
 
-    properties.url = url;
+    // price of listing
+    const priceElement = await page.waitForSelector('.price'); 
+    listing.price = await priceElement.evaluate(el => el.textContent);
+
+    // url of listing
+    listing.url = url;
 
     // description text from the listing
-    properties.info = await page.evaluate(() => {
+    listing.info = await page.evaluate(() => {
         let el = '';
         let i = 2;
         while(document.getElementById('postingbody').childNodes[i] != null) {
             el += document.getElementById('postingbody').childNodes[i].textContent;
             i++;
-        }
+        };
         return el;
-    })
-
-    console.log(properties.info + '\n' + '------------------------');
-
+    });
+    
+    // close second browser
     browser.close();
+
+    return listing;
 }
 
 async function startScraping() {
-    let job = new CronJob('* */30 * * * *', function() {
+    let job = new CronJob('* */30 * * * *', function() { // executes every 30 minutes
         getListings();
     }, null, true, null, null, true);
     job.start();
@@ -81,16 +96,25 @@ async function sendNotification(listings) {
         }
     });
 
-    let textBody = 'Craigslist Gaming PCs\n\n';
+    const craigslist = "---------------Craigslist Gaming PCs---------------";
+    //const facebook = "----------Facebook Marketplace Gaming PCs----------";
+    //const offerup = "-----------------OfferUp Gaming PCs-----------------";
+
+    let textBody = craigslist + '\n\n';
     
     listings.map(listing => {
         textBody += JSON.stringify(listing.title) + '\n';
         textBody += JSON.stringify(listing.price) + '\n'; 
-        textBody += JSON.stringify(listing.url) + '\n\n\n'; 
+        textBody += JSON.stringify(listing.url) + '\n'; 
+        const info = JSON.stringify(listing.info).split('\\n');
+        info.map(line => {
+            textBody += line + '\n';
+        });
+        textBody += '----------------------------------------------------\n\n\n'; 
     });
 
     let info = await transporter.sendMail({
-        from: '"New Listings!" ',
+        from: '"New Listings!"',
         to: '',
         subject: 'Gaming PCs',
         text: textBody
