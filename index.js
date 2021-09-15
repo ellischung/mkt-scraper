b895cc0fd324cf570f72e607eb0584e199704a30
Ellis <ellischung98@gmail.com>
2021-09-14 22:49:18 -0400

    fixed issue with retrieving text from listing's posting body

--- index.js
+++ index.js
@@ -7,25 +7,60 @@ async function getListings() {
 
     const page = await browser.newPage();
 
+    // link to search results
     const url = 'https://newyork.craigslist.org/d/for-sale/search/sss?max_price=500&query=gaming%20pc&sort=rel';
 
     await page.goto(url);
 
     await page.waitForSelector('.result-row');
 
+    // return the URLs for the listings
     const results = await page.$$eval('.result-row', rows => {
         return rows.map(row => {
             const properties = {};
             const titleElement = row.querySelector('.result-title');
-            properties.title = titleElement.innerText;
+            //properties.title = titleElement.innerText;
+            //const priceElement = row.querySelector('.result-price');
+            //properties.price = priceElement ? priceElement.innerText : '';
             properties.url = titleElement.getAttribute('href');
-            const priceElement = row.querySelector('.result-price');
-            properties.price = priceElement ? priceElement.innerText : '';
             return properties;
         });
+    });
+
+    // close first browser
+    browser.close();
+
+    await Promise.all(results.slice(0,5).map(async result => {
+        getListingInfo(result.url);
+    }));
+
+    //sendNotification(results.slice(0,5));
+}
+
+async function getListingInfo(url) {
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    // holds all important information from the listing
+    const properties = {};
+
+    properties.url = url;
+
+    // description text from the listing
+    properties.info = await page.evaluate(() => {
+        let el = '';
+        let i = 2;
+        while(document.getElementById('postingbody').childNodes[i] != null) {
+            el += document.getElementById('postingbody').childNodes[i].textContent;
+            i++;
+        }
+        return el;
     })
 
-    sendNotification(results.slice(0,5));
+    console.log(properties.info + '\n' + '------------------------');
 
     browser.close();
 }
@@ -50,12 +85,12 @@ async function sendNotification(listings) {
     
     listings.map(listing => {
         textBody += JSON.stringify(listing.title) + '\n';
-        textBody += JSON.stringify(listing.url) + '\n'; 
-        textBody += JSON.stringify(listing.price) + '\n\n\n'; 
-    })
+        textBody += JSON.stringify(listing.price) + '\n'; 
+        textBody += JSON.stringify(listing.url) + '\n\n\n'; 
+    });
 
     let info = await transporter.sendMail({
-        from: '"New Listings!" <fordevemailacc@gmail.com>',
+        from: '"New Listings!" ',
         to: '',
         subject: 'Gaming PCs',
         text: textBody
